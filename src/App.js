952dab0398fbd6873bccb359de27ae749d02bd2d
salauddinAmir <mdsalauddinahmed.amir@gmail.com>
2022-10-26 23:58:30 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
  
 import './App.css';
 import Header from './components/Header/header';
+import Shop from './components/Shop/shop';
 
 function App() {
   return (
     <div>
       <Header/>
+       <Shop/>
     </div>
   );
 }
