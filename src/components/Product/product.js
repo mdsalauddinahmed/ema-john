c828e35b8e0c69116ac9cbb4301f0f601b468c5f
salauddinAmir <mdsalauddinahmed.amir@gmail.com>
2022-10-28 00:40:33 +0600

    modeule 9

--- src/components/Product/product.js
+++ src/components/Product/product.js
@@ -1,15 +1,20 @@
 import React from 'react';
-
+import   './product.css';
 const Product = (props) => {
     //const img = props.product;
-    const {img,name}=props.product;
+    const {img,name,seller,price,stock}=props.product;
     return (
         <div className='product'>
             <div>
                 <img src={img} alt="" />
             </div>
             <div>
-            <h2>{name}</h2>
+             <h4 className='product-details'>{name}</h4>
+             <br />
+             <p><small>by : {seller}</small></p>
+             <p>${price}</p>
+             <p><small>Only {stock} left in stock- order soon</small></p>
+             <br />
             </div>
       
         </div>
